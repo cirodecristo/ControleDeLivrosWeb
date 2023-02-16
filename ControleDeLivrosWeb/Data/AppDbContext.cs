5c02216876df7037beabf20b14f5cd97bba947ed
Ciro Mauricio de Cristo <98125618+cirodecristo@users.noreply.github.com>
2023-02-16 11:06:09 -0300

    Adicionar arquivos de projeto.

---
+++ ControleDeLivrosWeb/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using CadastroDeLivrosWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CadastroDeLivrosWeb.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) :base(options)
+        {
+                
+        }
+
+        public DbSet<Categoria> Categorias { get; set; } 
+    }
+
+}
---
+++ ControleDeLivrosWeb/Models/Categoria.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CadastroDeLivrosWeb.Models
+{
+    public class Categoria
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Nome { get; set; }
+        public int OrdemMostra { get; set; }
+        public DateTime DataHoraCriacao { get; set; } = DateTime.Now;
+
+        public int casa { get; set; }
+        
+    }
+}
+
